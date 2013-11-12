441b732600444f029ba7c05653d1325762a84cee
kylehuff <code@curetheitch.com>
2013-11-11 21:05:12 -0500

    Added regex to replace http w/ https

--- build.sh
+++ build.sh
@@ -7,3 +7,7 @@ else
 fi
 
 jsduck -v --config config.json --output $OUTDIR && cp -rv patch/* $OUTDIR
+
+# Replace any occurrences of the non-https video player
+find $OUTDIR/. -name "*.htm" -o -name "*.html" -o -name "*.js" | xargs \
+  sed -i 's/http:\/\/player\.vimeo/https:\/\/player\.vimeo/g'
