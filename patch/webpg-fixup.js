aa84b2f3d417b0b9eb0e324cf8e9223c70526b15
kylehuff <code@curetheitch.com>
2014-02-02 12:38:29 -0500

    Fixup header(s) when in WebPG extension iframe

--- patch/webpg-fixup.js
+++ patch/webpg-fixup.js
@@ -1,5 +1,20 @@
 (function(){
-  Docs.initEventTracking = function() { Ext.getCmp('north-region').setHeight(80); Ext.getCmp('north-region').items.first().setHeight(53); };
+  Docs.initEventTracking = function() {
+    if (window.top === window) {
+      // Adjust the header size to the appropriate value
+      Ext.getCmp('north-region').setHeight(80);
+      Ext.getCmp('north-region').items.first().setHeight(53);
+    } else {
+      // Adjust the header for display in the WebPG extension iframe
+      Ext.getCmp('north-region').setHeight(32);
+      Ext.getCmp('north-region').items.first().setHeight(32);
+      Ext.getCmp('doctabs').setPosition(0, 5);
+      Ext.getCmp('doctabs').getEl(".tab-overflow").dom.style.top = '5px';
+      Ext.getCmp('search-container').el.dom.style.marginLeft = '-10px';
+      Ext.getCmp('search-container').el.dom.style.zIndex = '9999';
+      Ext.getElementById("header-content").style.display = 'none';
+    }
+  };
   var protocol = (document.location.protocol === "https:") ? "https:" : "http:";
   document.write("<link href='"+protocol+"//fonts.googleapis.com/css?family=Exo' rel='stylesheet' type='text/css' />");
 })();
