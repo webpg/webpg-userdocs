b029c622cced36dae06c47a9dd7bd63f3ca396e0
kylehuff <code@curetheitch.com>
2014-02-02 12:32:49 -0500

    Moved inline 'fixup' script to external file

---
+++ patch/webpg-fixup.js
@@ -0,0 +1,5 @@
+(function(){
+  Docs.initEventTracking = function() { Ext.getCmp('north-region').setHeight(80); Ext.getCmp('north-region').items.first().setHeight(53); };
+  var protocol = (document.location.protocol === "https:") ? "https:" : "http:";
+  document.write("<link href='"+protocol+"//fonts.googleapis.com/css?family=Exo' rel='stylesheet' type='text/css' />");
+})();
