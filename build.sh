63d79133f8fd9a88ec1850ad53f4d9ea69bccc48
kylehuff <code@curetheitch.com>
2014-05-03 12:34:01 -0400

    Have build script modify UI basics

--- build.sh
+++ build.sh
@@ -11,3 +11,10 @@ jsduck -v --config config.json --output $OUTDIR && cp -rv patch/* $OUTDIR
 # Replace any occurrences of the non-https video player
 find $OUTDIR/. -name "*.htm" -o -name "*.html" -o -name "*.js" | xargs \
   sed -i 's/http:\/\/player\.vimeo/https:\/\/player\.vimeo/g'
+
+# Swap navigation tree to default un-expanded
+find $OUTDIR/. -name "*.js" | xargs \
+  sed -i 's/expanded:true/expanded:false/g'
+
+#find $OUTDIR/. -name "*.htm" -o -name "*.html" -o -name "*.js" | xargs \
+#  sed -i 's/localStorage/myLocalStorage/g;s/cookie/myCookie/g'
