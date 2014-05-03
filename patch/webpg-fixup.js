42932ab4457adc83d3be3103c331216fde6aa60f
kylehuff <code@curetheitch.com>
2014-05-03 12:44:09 -0400

    Add WebPG specific fixup scripts/css

--- patch/webpg-fixup.js
+++ patch/webpg-fixup.js
@@ -1,21 +1,59 @@
+window.myLocalStorage = {};
+document.myCookie = function(val) {
+  var theCookie = document.cookie;
+  theCookie = val;
+  return theCookie;
+}
+
 (function(){
+  Ext.onReady(function() {
+	  // Open external links in new window.
+	  Ext.getBody().on('click', function(evt, target) {
+		  target = Ext.get(target);
+		  if (!target.is('a'))
+			  return;
+
+		  var href = target.getAttribute('href');
+		  if (href && !/^#/.exec(href) && window.open(href, '_blank'))
+			  evt.preventDefault();
+
+	  });
+	});
+
   Docs.initEventTracking = function() {
+    var doctabs = Ext.getCmp('doctabs'),
+        doctabs_top = 5,
+        header = Ext.getElementById("header-content"),
+        north = Ext.getCmp('north-region'),
+        search = Ext.getCmp('search-container');
+
     if (window.top === window) {
       // Adjust the header size to the appropriate value
-      Ext.getCmp('north-region').setHeight(80);
-      Ext.getCmp('north-region').items.first().setHeight(53);
+      north.setHeight(80);
+      north.items.first().setHeight(53);
     } else {
+      doctabs.on("move", function(e) {
+        var newPos = e.getPosition();
+        if (newPos.hasOwnProperty(1) &&
+            newPos[1] !== doctabs_top)
+          doctabs.setPosition(0, doctabs_top);
+      });
       // Adjust the header for display in the WebPG extension iframe
-      Ext.getCmp('north-region').setHeight(32);
-      Ext.getCmp('north-region').items.first().setHeight(32);
-      Ext.getCmp('doctabs').setPosition(0, 5);
-      Ext.getCmp('doctabs').el.dom.style.top = '5px !important';
-      Ext.getCmp('doctabs').getEl(".tab-overflow").dom.style.top = '5px';
-      Ext.getCmp('search-container').el.dom.style.marginLeft = '-10px';
-      Ext.getCmp('search-container').el.dom.style.zIndex = '9999';
-      Ext.getElementById("header-content").style.display = 'none';
+      north.setHeight(32);
+      north.items.first().setHeight(32);
+      doctabs.setPosition(0, 5);
+      doctabs.el.dom.style.top = '5px !important';
+      doctabs.getEl(".tab-overflow").dom.style.top = '5px';
+      search.el.dom.style.marginLeft = '-10px';
+      search.el.dom.style.zIndex = '9999';
+      header.style.display = 'none';
     }
   };
+//  Docs.otherProducts = [
+//      {text: 'Browser Extension', href: '../docs/index.html', icon: 'resources/images/flogo.png'},
+//      {text: 'libwebpg', href: 'http://webpg.org/docs/webpg-npapi/'}
+//  ];
+
   var protocol = (document.location.protocol === "https:") ? "https:" : "http:";
   document.write("<link href='"+protocol+"//fonts.googleapis.com/css?family=Exo' rel='stylesheet' type='text/css' />");
 })();
